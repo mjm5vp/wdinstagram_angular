f972dfdf69e9fc66da158513d0777d52b19e3c14
Mark Moeller <MarkMoellerUVA@gmail.com>
2017-06-21 11:12:26 -0400

    CRUD working

--- js/app.js
+++ js/app.js
@@ -32,14 +32,13 @@ var entryData = [
 
 // (function(){
   angular
-    .module("wdinstagram", ["ui.router"])
-    .config([
-      "$stateProvider",
-      RouterFunction
-    ])
-    .controller("InstaIndexController", [InstaIndexControllerFunction])
-    .controller("InstaShowController",["$stateParams",InstaShowControllerFunction
-    ])
+    .module("wdinstagram", ["ui.router", "ngResource"])
+    .config(["$stateProvider", RouterFunction])
+    .controller("InstaIndexController", ["InstaFactory", InstaIndexControllerFunction])
+    .controller("InstaNewController", ["InstaFactory", InstaNewControllerFunction])
+    .controller("InstaEditController", ["InstaFactory", "$stateParams", InstaEditControllerFunction])
+    .controller("InstaShowController",["InstaFactory", "$stateParams",InstaShowControllerFunction])
+    .factory("InstaFactory", ["$resource", InstaFactoryFunction])
 
   function RouterFunction($stateProvider){
     console.log("router working")
@@ -50,21 +49,59 @@ var entryData = [
       controller: "InstaIndexController",
       controllerAs: "vm"
     })
+    .state("instaNew", {
+      url: "/instas/new",
+      templateUrl: "js/ng-views/new.html",
+      controller: "InstaNewController",
+      controllerAs: "vm"
+    })
+    .state("instaEdit", {
+      url: "/instas/:id/edit",
+      templateUrl: "js/ng-views/edit.html",
+      controller: "InstaEditController",
+      controllerAs: "vm"
+    })
     .state("instaShow",{
       url: "/instas/:id",
       templateUrl: "js/ng-views/ng-show.html",
       controller: "InstaShowController",
       controllerAs: "vm"
     })
+
   }
 
-  function InstaIndexControllerFunction(){
+  function InstaIndexControllerFunction(InstaFactory){
     console.log("controller working")
-    this.instas = entryData
+    this.instas = InstaFactory.query()
+    console.log(this.instas)
   }
 
-  function InstaShowControllerFunction($stateParams){
-    this.insta = entryData[$stateParams.id]
+  function InstaNewControllerFunction(InstaFactory){
+    this.insta = new InstaFactory()
+    this.create = function(){
+      this.insta.$save()
+    }
+  }
+
+  function InstaShowControllerFunction(InstaFactory, $stateParams){
+    this.insta = InstaFactory.get({id: $stateParams.id})
+  }
+
+  function InstaEditControllerFunction(InstaFactory, $stateParams){
+    this.insta = InstaFactory.get({id: $stateParams.id})
+    console.log(this.insta)
+    this.update = function(){
+      this.insta.$update({id: $stateParams.id})
+    }
+    this.destroy = function(){
+      this.insta.$delete({id: $stateParams.id})
+    }
+  }
+
+  function InstaFactoryFunction($resource){
+    return $resource("http://localhost:3000/entries/:id", {}, {
+        update: { method: "PUT" }
+      });
   }
 
 
